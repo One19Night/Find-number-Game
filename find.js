28617410fe91e4beeecf1bb434c3f989b5225a6e
One19Night <92639620+One19Night@users.noreply.github.com>
2022-01-18 09:32:46 +0700

    C2 - Làm việc với HTML

--- find.js
+++ find.js
@@ -1,4 +1,23 @@
 "use strick";
-var age = 25 
-var age = 14        
-console.log(age) 
+var total = Horizontal*Column 
+
+function creatnumber(){
+    //tạo mảng 
+    for(k=0;k<=total;k++){
+
+    }
+    //thiết lập vị trí 
+    for(i=0;i<=Horizontal; i++){
+        for(j=0;j<=Column; j++){
+            Math.random()*total 
+        }
+    }
+}
+
+function findnumber(){
+
+}
+
+function score(){
+    
+}
