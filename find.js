8e58c1d0b9ec53934e3c60c6a1152ec9ffb4929c
One19Night <92639620+One19Night@users.noreply.github.com>
2022-01-19 10:05:12 +0700

    C4 - Lấy dữ liệu HTML với button

--- find.js
+++ find.js
@@ -1,26 +1,35 @@
 "use strick";
-import '.index.html'
-var Total = Horizontal*Column 
-var Horizontal = document.getElementById("Horizontal").value;
-var Column = document.getElementById("Column").value;
-console.log(Column)
-function creatnumber(){
-    //tạo mảng 
-    for(k=0;k<=total;k++){
-
-    }
-    //thiết lập vị trí 
-    for(i=0;i<=Horizontal; i++){
-        for(j=0;j<=Column; j++){
-            Math.random()*total 
-        }
-    }
+  function Send() {
+    const x = document.forms["frm1"];
+    let horizontal = parseInt(x.elements[0].value)
+    let column = parseInt(x.elements[1].value)
+    let multiplication = parseInt(x.elements[2].value)
+    //console.log(horizontal, column , multiplication) 
+    let number=[]
+for (i=1 ; i<=multiplication ; i++)
+{
+  number.push(i)
+}
+//console.log(number)
 }
 
-function findnumber(){
+// function creatnumber(){
+//     //tạo mảng 
+//     for(k=0;k<=total;k++){
 
-}
+//     }
+//     //thiết lập vị trí 
+//     for(i=0;i<=Horizontal; i++){
+//         for(j=0;j<=Column; j++){
+//             Math.random()*total 
+//         }
+//     }
+// }
 
-function score(){
+// function findnumber(){
 
-}
+// }
+
+// function score(){
+
+// }
