93155a06ec9a3a07f5d654e93359eb706c398f25
One19Night <92639620+One19Night@users.noreply.github.com>
2022-01-19 10:05:12 +0700

    C4 - Lấy dữ liệu HTML với button

--- find.js
+++ find.js
@@ -1,9 +1,8 @@
 "use strick";
-import '.index.html'
-var Total = Horizontal*Column 
-var Horizontal = document.getElementById("Horizontal").value;
-var Column = document.getElementById("Column").value;
-console.log(Column)
+// var Total = Horizontal*Column 
+// var Horizontal = document.getElementById("Horizontal").value;
+// var Column = document.getElementById("Column").value;
+// console.log(Column)
 function creatnumber(){
     //tạo mảng 
     for(k=0;k<=total;k++){
